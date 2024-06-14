dda59cb8dff02025588a30a40a6fbaeca632cce2
Abe <abrahamgeorge8547@gmail.com>
2024-06-14 11:33:59 +0530

    first milestone

---
+++ src/lib/popup.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let name: string;
+    
+  </script>
+  
+  <main>
+    <h1 class="bg-red-800">Hello {name} !!</h1>
+    
+  </main>
+  
---
+++ src/popup.ts
@@ -0,0 +1,10 @@
+import App from "./lib/popup.svelte";
+import "./tailwind.css";
+const app = new App({
+    target: document.body,
+    props: {
+        name: "world",
+    }
+});
+
+export default app;
