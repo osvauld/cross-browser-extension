b009c3cb6e9b93e6b7b2d5f6b8b6f6797ae0be8a
abe <abe@osvauld.com>
2024-06-23 20:43:20 +0530

    package ignore

--- src/scripts/wasm.js
+++ src/scripts/wasm.js
@@ -13,10 +13,10 @@ const cachedTextDecoder =
 	typeof TextDecoder !== "undefined"
 		? new TextDecoder("utf-8", { ignoreBOM: true, fatal: true })
 		: {
-			decode: () => {
-				throw Error("TextDecoder not available");
-			},
-		};
+				decode: () => {
+					throw Error("TextDecoder not available");
+				},
+			};
 
 if (typeof TextDecoder !== "undefined") {
 	cachedTextDecoder.decode();
